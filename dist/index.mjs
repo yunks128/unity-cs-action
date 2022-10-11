81101b789971eed5eb759ce57a30854a7fcc8952
Tom Barber <tom@spicule.co.uk>
2022-10-11 10:46:30 +0000

    update imports

--- dist/index.mjs
+++ dist/index.mjs
@@ -9571,49 +9571,6 @@ module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("zlib");
 
 /***/ }),
 
-/***/ 6952:
-/***/ ((__webpack_module__, __unused_webpack___webpack_exports__, __nccwpck_require__) => {
-
-__nccwpck_require__.a(__webpack_module__, async (__webpack_handle_async_dependencies__) => {
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(9262);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(8051);
-//const core = require('@actions/core');
-//const github = require('@actions/github');
-
-
-
-
-try {
-    let meta = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('ucsmetadata');
-    let token = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('token')
-    if(meta === undefined || meta.length < 2){
-        meta = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('eksmetadata')
-        if(meta === undefined || meta.length < 2){
-            _actions_core__WEBPACK_IMPORTED_MODULE_1__.setFailed('No metadata found')
-        }
-    }
-    console.log(`Found meta ${meta}!`);
-    const [owner, repo] = "unity-sds/unity-cs-action".split("/");
-    let octokit = _actions_github__WEBPACK_IMPORTED_MODULE_0__.getOctokit(token)
-    const { data } = await  octokit.request("POST /repos/{owner}/{repo}/issues", {
-        owner,
-        repo,
-        title: "My test issue",
-    });
-    console.log("Issue created: %s", data.html_url);
-    const time = (new Date()).toTimeString();
-    _actions_core__WEBPACK_IMPORTED_MODULE_1__.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(_actions_github__WEBPACK_IMPORTED_MODULE_0__.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    _actions_core__WEBPACK_IMPORTED_MODULE_1__.setFailed(error.message);
-}
-__webpack_handle_async_dependencies__();
-}, 1);
-
-/***/ }),
-
 /***/ 2020:
 /***/ ((module) => {
 
@@ -9654,89 +9611,70 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ }
 /******/ 
 /************************************************************************/
-/******/ /* webpack/runtime/async module */
-/******/ (() => {
-/******/ 	var webpackThen = typeof Symbol === "function" ? Symbol("webpack then") : "__webpack_then__";
-/******/ 	var webpackExports = typeof Symbol === "function" ? Symbol("webpack exports") : "__webpack_exports__";
-/******/ 	var completeQueue = (queue) => {
-/******/ 		if(queue) {
-/******/ 			queue.forEach((fn) => (fn.r--));
-/******/ 			queue.forEach((fn) => (fn.r-- ? fn.r++ : fn()));
-/******/ 		}
-/******/ 	}
-/******/ 	var completeFunction = (fn) => (!--fn.r && fn());
-/******/ 	var queueFunction = (queue, fn) => (queue ? queue.push(fn) : completeFunction(fn));
-/******/ 	var wrapDeps = (deps) => (deps.map((dep) => {
-/******/ 		if(dep !== null && typeof dep === "object") {
-/******/ 			if(dep[webpackThen]) return dep;
-/******/ 			if(dep.then) {
-/******/ 				var queue = [];
-/******/ 				dep.then((r) => {
-/******/ 					obj[webpackExports] = r;
-/******/ 					completeQueue(queue);
-/******/ 					queue = 0;
-/******/ 				});
-/******/ 				var obj = {};
-/******/ 											obj[webpackThen] = (fn, reject) => (queueFunction(queue, fn), dep['catch'](reject));
-/******/ 				return obj;
-/******/ 			}
-/******/ 		}
-/******/ 		var ret = {};
-/******/ 							ret[webpackThen] = (fn) => (completeFunction(fn));
-/******/ 							ret[webpackExports] = dep;
-/******/ 							return ret;
-/******/ 	}));
-/******/ 	__nccwpck_require__.a = (module, body, hasAwait) => {
-/******/ 		var queue = hasAwait && [];
-/******/ 		var exports = module.exports;
-/******/ 		var currentDeps;
-/******/ 		var outerResolve;
-/******/ 		var reject;
-/******/ 		var isEvaluating = true;
-/******/ 		var nested = false;
-/******/ 		var whenAll = (deps, onResolve, onReject) => {
-/******/ 			if (nested) return;
-/******/ 			nested = true;
-/******/ 			onResolve.r += deps.length;
-/******/ 			deps.map((dep, i) => (dep[webpackThen](onResolve, onReject)));
-/******/ 			nested = false;
-/******/ 		};
-/******/ 		var promise = new Promise((resolve, rej) => {
-/******/ 			reject = rej;
-/******/ 			outerResolve = () => (resolve(exports), completeQueue(queue), queue = 0);
-/******/ 		});
-/******/ 		promise[webpackExports] = exports;
-/******/ 		promise[webpackThen] = (fn, rejectFn) => {
-/******/ 			if (isEvaluating) { return completeFunction(fn); }
-/******/ 			if (currentDeps) whenAll(currentDeps, fn, rejectFn);
-/******/ 			queueFunction(queue, fn);
-/******/ 			promise['catch'](rejectFn);
-/******/ 		};
-/******/ 		module.exports = promise;
-/******/ 		body((deps) => {
-/******/ 			if(!deps) return outerResolve();
-/******/ 			currentDeps = wrapDeps(deps);
-/******/ 			var fn, result;
-/******/ 			var promise = new Promise((resolve, reject) => {
-/******/ 				fn = () => (resolve(result = currentDeps.map((d) => (d[webpackExports]))));
-/******/ 				fn.r = 0;
-/******/ 				whenAll(currentDeps, fn, reject);
-/******/ 			});
-/******/ 			return fn.r ? promise : result;
-/******/ 		}).then(outerResolve, reject);
-/******/ 		isEvaluating = false;
-/******/ 	};
-/******/ })();
-/******/ 
 /******/ /* webpack/runtime/compat */
 /******/ 
 /******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
 /******/ 
 /************************************************************************/
-/******/ 
-/******/ // startup
-/******/ // Load entry module and return exports
-/******/ // This entry module used 'module' so it can't be inlined
-/******/ var __webpack_exports__ = __nccwpck_require__(6952);
-/******/ __webpack_exports__ = await __webpack_exports__;
-/******/ 
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(9262);
+/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(8051);
+//const core = require('@actions/core');
+//const github = require('@actions/github');
+
+
+
+
+function spinUpEKS(meta) {
+    if(meta.hasOwnProperty("extensions")){
+        if(meta["extensions"].hasOwnProperty("kubernetes")){
+            console.log("call eks workflow")
+        }
+    } else {
+        console.log("call eks workflow 2")
+    }
+    
+}
+
+function spinUpProjects(meta, token) {
+    if(meta["services"]){
+        meta["services"].forEach(function (item, index) {
+            console.log("Service found")
+            console.log(item, index);
+        });
+    }
+}
+
+try {
+    let meta = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('ucsmetadata');
+    let token = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('token')
+    if(meta === undefined || meta.length < 2){
+        meta = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('eksmetadata')
+        if(meta === undefined || meta.length < 2){
+            _actions_core__WEBPACK_IMPORTED_MODULE_1__.setFailed('No metadata found')
+        }
+    }
+    console.log(`Found meta ${meta}!`);
+    // const [owner, repo] = "unity-sds/unity-cs-action".split("/");
+    // let octokit = github.getOctokit(token)
+    // const { data } = await  octokit.request("POST /repos/{owner}/{repo}/issues", {
+    //     owner,
+    //     repo,
+    //     title: "My test issue",
+    // });
+    const metaobj = JSON.parse(meta)
+    spinUpEKS(metaobj, token)
+    spinUpProjects(metaobj, token)
+    // console.log("Issue created: %s", data.html_url);
+    const time = (new Date()).toTimeString();
+    _actions_core__WEBPACK_IMPORTED_MODULE_1__.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(_actions_github__WEBPACK_IMPORTED_MODULE_0__.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    _actions_core__WEBPACK_IMPORTED_MODULE_1__.setFailed(error.message);
+}
+})();
+
