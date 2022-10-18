74f3784933bbc7efbf863f8d30540cb673f0b54c
Tom Barber <tom@spicule.co.uk>
2022-10-18 16:06:03 +0000

    update inputs

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import {runWF} from "./return-dispatch/main"
 import {runWait} from "./await-remote-run/main";
 import {ActionWorkflowInputs} from "./return-dispatch/action";
 
-async function spinUpEKS(meta: MetaObject, token: string) {
+async function spinUpEKS(meta: MetaObject, token: string, awskey: string, awssecret: string, awstoken: string) {
     if (meta.hasOwnProperty("extensions")) {
         if (meta["extensions"].hasOwnProperty("kubernetes")) {
             console.log("call eks workflow")
             const input: ActionWorkflowInputs = {
-            "metadata":JSON.stringify(meta.extensions.kubernetes)
+            "metadata":JSON.stringify(meta.extensions.kubernetes),
+                "KEY": awskey,
+                "SECRET": awssecret,
+                "TOKEN": awstoken,
             }
             let id: number = await runWF("unity-sds",
                 "refs/heads/main",
@@ -29,18 +32,39 @@ async function spinUpEKS(meta: MetaObject, token: string) {
 
 }
 
-function spinUpProjects(meta: MetaObject, token: string) {
+async function spinUpProjects(meta: MetaObject, token: string) {
     if (meta["services"]) {
-        meta["services"].forEach(function (item, index) {
+        for (const item of meta["services"]) {
+            const index = meta["services"].indexOf(item);
             console.log("Service found")
             console.log(item, index);
-        });
+            console.log("call service workflow")
+            const input: ActionWorkflowInputs = {
+                "deploymentTarget": "mcp",
+                "sourceRepository": item.source,
+                "sourceBranch": item.branch
+            }
+            let id: number = await runWF("unity-sds",
+                "refs/heads/main",
+                "unity-cs-infra",
+                token,
+                "deployment.yml",
+                1800,
+                input
+            )
+            console.log("checking run")
+            await runWait("unity-sds", 5000, "unity-cs-infra", id, 3600, token)
+            console.log("wf id: " + id)
+        }
     }
 }
 
 async function run(): Promise<void> {
     let meta = core.getInput('ucsmetadata');
     let token = core.getInput('token')
+    let awskey = core.getInput('awskey')
+    let awstoken = core.getInput('awstoken')
+    let awssecret = core.getInput('awssecret')
     if (meta === undefined || meta.length < 2) {
         meta = core.getInput('eksmetadata')
         if (meta === undefined || meta.length < 2) {
@@ -49,7 +73,7 @@ async function run(): Promise<void> {
     }
     console.log(`Found meta ${meta}!`);
     const metaobj = JSON.parse(meta)
-    spinUpEKS(metaobj, token)
+    spinUpEKS(metaobj, token, awskey, awssecret, awstoken)
     spinUpProjects(metaobj, token)
     // console.log("Issue created: %s", data.html_url);
     const time = (new Date()).toTimeString();
