448106ad7c7e48ab608bc2bd0650307ce6f03924
Barber <barber@MT-306070.jpl.nasa.gov>
2023-03-21 15:50:25 +0000

    add test test

---
+++ src/meta.spec.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+describe("utils", () => {
+  describe("getBranchNameFromRef", () => {
+    it("should return the branch name for a valid branch ref", () => {
+      expect("").toStrictEqual("");
+    });
+   });
+});
